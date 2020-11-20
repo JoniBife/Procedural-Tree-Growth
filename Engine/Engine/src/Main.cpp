898823a0d10844c9c3e0f451e562b423b401b769
João <jonisilva@live.com.pt>
2020-11-20 19:57:52 +0000

    avt challenge

--- Engine/Engine/src/Main.cpp
+++ Engine/Engine/src/Main.cpp
@@ -1,11 +1,11 @@
 #include "Engine.h"
 #include <iostream>
-#include "apps/AppCGJ.h"
-//#include "apps/AppAVT.h"
+//#include "apps/AppCGJ.h"
+#include "apps/AppAVT.h"
 
 int main(int argc, char* argv[])
 {
-	Engine* eng = &AppCGJ();
+	Engine* eng = &AppAVT();
 	eng->run();
 	exit(EXIT_SUCCESS);
 }
